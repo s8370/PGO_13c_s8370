6846a04c7c6949614b9e4ed7d2f64f7fbabd93e6
s8370 <s8370@pjwstk.edu.pl>
2022-03-13 17:53:52 +0100

    task created

--- s8370/src/g13c/cw1/zad1/zad1.java
+++ s8370/src/g13c/cw1/zad1/zad1.java
@@ -1,19 +1,9 @@
 package g13c.cw1.zad1;
-import java.util.Scanner;
 
 public class zad1 {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Podaj a:");
-    int a = scanner.nextInt();
-    System.out.println("Podaj b:");
-    int b = scanner.nextInt();
-    System.out.println("Podaj c:");
-    int c = scanner.nextInt();
-    if(a <= b && a<= c){
-        System.out.println("a: "+a);
-        if(b <= c) System.out.println("b: "+b);
-        else System.out.println("c: "+c);
-    } else {
-        System.out.println("reszta...");
+
+    public static void main(String[] args) {
+        // GITHUB ONLY
     }
+
 }
